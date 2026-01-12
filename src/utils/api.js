ba867b995f2ae00fe608513fc448e74a0715a580
Saksham Jain <sakshamjain7680@gmail.com>
2026-01-13 02:25:59 +0530

    Fix: use VITE_API_URL instead of hardcoded backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://gigflow-servicehive-f4jt.onrender.com/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
 
