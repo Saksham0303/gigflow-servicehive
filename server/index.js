fce8100c8c2c15911def04a65afb2155f7b7a6d8
Saksham Jain <sakshamjain7680@gmail.com>
2026-01-13 00:12:36 +0530

    Final fix: CORS with credentials + preflight

--- server/index.js
+++ server/index.js
@@ -9,15 +9,32 @@ const gigsRoutes = require('./routes/gigs');
 const bidsRoutes = require('./routes/bids');
 
 const app = express();
-
 connectDB();
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://gigflow-servicehive-j8hbuiyvi-saksham-jains-projects-4a0a7f8e.vercel.app',
+];
 
 app.use(
   cors({
-    origin: true,
+    origin: function (origin, callback) {
+      
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(null, false); 
+      }
+    },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
+
+app.options('*', cors());
 
 app.use(express.json());
 app.use(cookieParser());
